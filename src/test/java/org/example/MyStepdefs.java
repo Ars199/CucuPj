a8a3cb63d803d00c88c70444d41d518875225ddb
Ars199 <6653312@gmail.com>
2022-12-01 11:36:27 +0300

    updtd

--- src/test/java/org/example/MyStepdefs.java
+++ src/test/java/org/example/MyStepdefs.java
@@ -1,2 +1,30 @@
-package org.example;public class MyStepdefs {
+package org.example;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.ru.Дано;
+import cucumber.api.java.ru.Когда;
+import cucumber.api.java.ru.Тогда;
+
+public class MyStepdefs {
+
+    @Дано("^на счете пользователя имеется (\\d+) рублей$")
+    public void наСчетеПользователяИмеетсяРублей(int arg1) throws Throwable {
+
+    // Write code here that turns the phrase above into concrete actions
+
+
+        throw new PendingException();
+    }
+
+    @Когда("^пользователь снимает со счета (\\d+) рублей$")
+    public void пользовательСнимаетСоСчетаРублей(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Тогда("^появляется предупреждение \"([^\"]*)\"$")
+    public void появляетсяПредупреждение(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
 }
--- src/test/java/org/example/RunnerTest.java
+++ src/test/java/org/example/RunnerTest.java
@@ -1,2 +1,29 @@
-package org.example;public class RunnerTest {
+package org.example;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**!!внимание, название класса должно иметь окончание Test, иначе тесты не будут запускаться
+ * features – путь к папке с .feature файлами. Фреймворк будет искать файлы в этой и во всех дочерних папках. Можно указать несколько папок, например: features = {«src/test/features», «src/test/feat»};
+ * **/
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        /*features = "src/test/features",
+        glue = "ru.savkk.test",
+        tags = "@all",
+        dryRun = false,
+        strict = false,
+        snippets = SnippetType.UNDERSCORE
+//        name = "^Успешное|Успешная.*"*/
+
+                features = "src/test/features",
+                glue = "org.example",
+                tags = "@withdrawal",
+                snippets = SnippetType.CAMELCASE
+
+)
+public class RunnerTest {
 }
